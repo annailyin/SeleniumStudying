f79cf030487389998e8e22171b7a4d6bbc3448ce
Anna Ilyinkova <anna.ilyin@gmail.com>
2022-08-26 22:04:21 +0300

    Created needed classes for pages and test to run via NUnit.

--- Homework1/UnitTest1.cs
+++ Homework1/UnitTest1.cs
@@ -1,18 +1,33 @@
+﻿using MailRuClassLibrary;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Homework1
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        const string Username = "udodudodovich@mail.ru";
+        const string Password = "cxzASDewq123567";
+        const string Url = "https://mail.ru/";
+
+        By MailAddressSelector = By.XPath("//span[@class='ph-project__user-name svelte-1hiqrvn']");
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            WebDriver driver = new ChromeDriver { Url = Url };
+            driver.Manage().Window.Maximize();
+
+            MailRuHomePage homePage = new MailRuHomePage(driver);
+            homePage.Login(Username, Password);
+
+            var mailAddress = driver.FindElement(MailAddressSelector);
+            var value = mailAddress.Text;
+            Assert.AreEqual(Username, value);
+
+            driver.Close();
+            driver.Dispose();
         }
     }
 }
