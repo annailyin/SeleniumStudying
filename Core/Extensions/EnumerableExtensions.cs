6828bb1d647ac5dc3bad2bdd7859686f93fd0e75
Anna Ilyinkova <anna.ilyin@gmail.com>
2022-09-20 12:41:26 +0300

    Created automated scripts for the rest tasks.

--- Core/Extensions/EnumerableExtensions.cs
+++ Core/Extensions/EnumerableExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace YandexMailClassLibrary
+namespace Core.Extensions
 {
     public static class EnumerableExtensions
     {
--- YandexMailClassLibrary/YandexMailHomePage.cs
+++ YandexMailClassLibrary/YandexMailHomePage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Core.Extensions;
+using OpenQA.Selenium;
 
 namespace YandexMailClassLibrary
 {
